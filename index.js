a3cf3ad6018fc0d8caa91d924288236677634ebb
moin1990 <kemoin@gmail.com>
2026-05-21 22:24:17 +0600

    eight

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    process.env.CLIENT_URL || 'https://ideavault.vercel.app'
+    process.env.CLIENT_URL || 'https://drass94-client.vercel.app/'
   ],
   credentials: true
 }));
